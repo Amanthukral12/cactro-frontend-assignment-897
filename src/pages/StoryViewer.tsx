40fc81926999dd26326c3944407bdf44187ffb30
Aman Thukral <thukral618@gmail.com>
2025-05-04 20:49:35 +0530

    Add feature to auto move to next story after 5 seconds

--- src/pages/StoryViewer.tsx
+++ src/pages/StoryViewer.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import stories from "../constants/stories";
 import ThumbnailBar from "../components/ThumbnailBar";
 import StoryDisplay from "../components/StoryDisplay";
 const StoryViewer = () => {
   const [current, setCurrent] = useState<number | null>(null);
   const [loading, setLoading] = useState(false);
+  const timerRef = useRef<number | null>(null);
 
   const openStory = (index: number) => setCurrent(index);
 
   const closeStory = () => {
     setCurrent(null);
+    clearTimeout(timerRef.current!);
   };
 
   const showNextStory = () => {
@@ -29,12 +31,32 @@ const StoryViewer = () => {
       closeStory();
     }
   };
+
+  useEffect(() => {
+    if (current !== null) {
+      setLoading(true);
+      const img = new Image();
+      img.src = stories[current].imageUrl;
+      img.onload = () => {
+        setLoading(false);
+        timerRef.current = setTimeout(showNextStory, 5000);
+      };
+    }
+    return () => clearTimeout(timerRef.current!);
+  }, [current]);
   return (
-    <div>
+    <div
+      className={`flex flex-col items-center h-screen w-full p-2 ${
+        current !== null ? "p-0" : ""
+      }`}
+    >
       {current === null ? (
         <>
           <ThumbnailBar stories={stories} onSelect={openStory} />
-          <img src="/stories/1.jpg" />
+          <img
+            src="/stories/1.jpg"
+            className="w-full max-w-xs mt-4 rounded-lg object-cover"
+          />
         </>
       ) : (
         <StoryDisplay
